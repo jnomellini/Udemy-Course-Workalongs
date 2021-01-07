80d84771bcdfdc5040cbe0985946495966dc80bd
jnomellini <46978925+jnomellini@users.noreply.github.com>
2021-01-06 18:35:52 -0800

    Add files via upload

---
+++ Modulesandpackages.py
@@ -0,0 +1,28 @@
+# How to install external packages hosted on PyPi
+
+          
+C:\Usersjoenom>pip install requests
+
+C:\Usersjoenom>pip install colorama
+
+C:\Usersjoenom>Python
+
+    
+>>>from colorama import init
+>>>init()
+>>>from colorama import Fore
+>>>print("Fore.RED + some red text")
+some red text
+>>>print("Fore.GREEN + some green text")
+some green text     
+>>>quit()
+
+
+# Always try to google package names such as openpyxl, xlsxwriter etc....
+
+
+# Example on how to import Excel Python Package 
+
+C:\Usersjoenom>pip install openpyxl
+
+>>>import openpyxl
